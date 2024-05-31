0bbf933957eb35419e88d50156099f832b026b0a
eddie-gomes <edvan.bgjunior@gmail.com>
2024-05-31 00:59:35 -0300

    Isolando serviços de login
    Co-authored-by: Philipe Serafim <philipeserafim@gmail.com>

--- src/routes/router.js
+++ src/routes/router.js
@@ -32,21 +32,4 @@ routes.put('/cliente/:email', verificaToken, (req, res) =>
   clienteController.update(req, res),
 );
 
-// Rotas de Projeto
-routes.post('/projeto', verificaToken, (req, res) => ProjetoController.create(req, res));
-routes.get('/projeto', verificaToken, (req, res) => ProjetoController.list(req, res));
-routes.get('/projeto/:id', verificaToken, (req, res) => ProjetoController.getById(req, res));
-routes.put('/projeto/:id', verificaToken, (req, res) => ProjetoController.update(req, res));
-routes.delete('/projeto/:id', verificaToken, (req, res) => ProjetoController.delete(req, res));
-
-// Projetos - Rota sem autenticação para listar todos os projetos
-routes.get('/projetos/all', (req, res) => ProjetoController.listAll(req, res));
-
-// Projetos - Rota sem autenticação para listar os 3 primeiros projetos
-routes.get('/projetos/top3', (req, res) => ProjetoController.listTopThree(req, res));
-
-// Projetos - Rota sem autenticação para buscar projeto
-routes.get('/projetos/public/:id', (req, res) => ProjetoController.getByIdPublic(req, res));
-
-
 module.exports = routes;
